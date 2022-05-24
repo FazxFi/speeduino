45737614f9f53f754af23e744fdf36f1b007b027
FZL <muhammadfazlee38@gmail.com>
2022-05-24 20:43:37 +0800

    Fix stray aircon status stuff

--- speeduino/globals.h
+++ speeduino/globals.h
@@ -738,7 +738,6 @@ struct statuses {
   long vvt2Duty; //Has to be a long for PID calcs (CL VVT control)
   byte outputsStatus;
   byte TS_SD_Status; //TunerStudios SD card status
-  byte airConStatus; // The air conditioning status bits (see BIT_AIRCON_* defines at the top of this file.)
 };
 
 /** Page 2 of the config - mostly variables that are required for fuel.
