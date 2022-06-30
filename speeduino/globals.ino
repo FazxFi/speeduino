b00ff3ec0e7d9a746c6fa32072805fd563c23550
FZL <muhammadfazlee38@gmail.com>
2022-06-30 19:31:36 +0800

    Adapting to the New s

--- speeduino/globals.ino
+++ speeduino/globals.ino
@@ -281,7 +281,8 @@ inline bool pinIsOutput(byte pin)
 {
   bool used = false;
   bool isIdlePWM = (configPage6.iacAlgorithm > 0) && ((configPage6.iacAlgorithm <= 3) || (configPage6.iacAlgorithm == 6));
-  bool isIdleSteper = (configPage6.iacAlgorithm > 3) && (configPage6.iacAlgorithm != 6);
+  bool isIdleSteper = (configPage6.iacAlgorithm > 3) && (configPage6.iacAlgorithm < 6);
+  bool isIdleHB = (configPage6.iacAlgorithm >= 8);
   //Injector?
   if ((pin == pinInjector1)
   || ((pin == pinInjector2) && (configPage2.nInjectors > 1))
@@ -313,11 +314,13 @@ inline bool pinIsOutput(byte pin)
   || ((pin == pinVVT_1) && (configPage10.wmiEnabled > 0))
   || ((pin == pinVVT_2) && (configPage10.vvt2Enabled > 0))
   || ((pin == pinBoost) && (configPage6.boostEnabled == 1))
-  || ((pin == pinIdle1) && isIdlePWM)
+  || ((pin == pinIdle1) && (isIdlePWM || isIdleHB))
   || ((pin == pinIdle2) && isIdlePWM && (configPage6.iacChannels == 1))
   || ((pin == pinStepperEnable) && isIdleSteper)
   || ((pin == pinStepperStep) && isIdleSteper)
   || ((pin == pinStepperDir) && isIdleSteper)
+  || ((pin == pinHBdir1) && isIdleHB)
+  || ((pin == pinHBdir2) && isIdleHB)
   || (pin == pinTachOut) )
   {
     used = true;
